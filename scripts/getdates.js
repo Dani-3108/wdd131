bea1ea015d83299951d07f5343f0e27a8e4d1f50
Dani-3108 <floresd477@gmail.com>
2025-11-08 12:45:37 -0600

    javascript code added and final changes

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const currentyear = today.getFullYear();
+document.querySelector("#currentyear").textContent = currentyear;
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+
