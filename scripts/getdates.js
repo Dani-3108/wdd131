cf7558c4098784dd4b883d8f233d2242bad52b6d
Dani-3108 <floresd477@gmail.com>
2026-03-07 10:42:47 -0600

    W01 Assignment: Home Page Completed

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+//Variables
+const today = new Date();
+//getting the document
+document.getElementById("currentyear").innerHTML = today.getFullYear();
+document.getElementById("lastModified").innerHTML = document.lastModified;
